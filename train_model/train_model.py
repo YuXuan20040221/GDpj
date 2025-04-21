486f0d1f24e00fbcf06f94bc0e69ea9fd7fb429e
Lab <acs111111@gm.ntcu.edu.tw>
2025-04-21 16:36:27 +0800

    2025..4.21 - Yolov8n Part 2

--- train_model/train_model.py
+++ train_model/train_model.py
@@ -9,7 +9,7 @@ model = YOLO("yolov8n.pt")
 # 設定訓練參數
 model.train(
     data="datasets/train.yaml",  # 數據集設定
-    epochs=20,          # 訓練回合數
+    epochs=300,          # 訓練回合數
     imgsz=640,          # 圖片大小
     project="model",    # 變更儲存目錄
     name="yolov8n_custom"  # 設定模型名稱
